e6117260cfc800e5497e81ddfa1664b6ede67601
s4nt14go <santiagopergamo@gmail.com>
2020-10-12 11:31:31 -0300

    Revert "Add like API"
    This reverts commit 415d9cc7

--- services/like-api/like.js
+++
@@ -1,7 +0,0 @@
-import { success } from "../../libs/response-lib";
-
-export async function main(event, context) {
-  // Business logic code for liking a post
-
-  return success({ status: true });
-}
